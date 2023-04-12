a968ee9033c2a56d78c57292465227f19df14b9f
Juliane Cassidy <julianecassidy@gmail.com>
2023-04-12 12:18:40 -0600

    add tests for slq.js

--- helpers/sql.js
+++ helpers/sql.js
@@ -1,6 +1,17 @@
 const { BadRequestError } = require("../expressError");
 
-// THIS NEEDS SOME GREAT DOCUMENTATION.
+/** Converts data object to SQL compatible object with parameterization.
+ * 
+ *  Takes dataToUpdate object and jsToSql object which assigns camel case 
+ *  column names to snake case column names.
+ * 
+ *  Returns object with parametized table column names as keys and values to
+ *  insert as values.
+ * 
+ *  Ex. {name: "Apple", description: "Computers.", numEmployees: 100}, 
+ *      {numEmployees: "num_employees"}
+ * --> {"name=$1": "Apple", "description=$2": "Computers.", "num_employees=$3": 100}
+ */ 
 
 function sqlForPartialUpdate(dataToUpdate, jsToSql) {
   const keys = Object.keys(dataToUpdate);
---
+++ helpers/sql.test.js
@@ -0,0 +1,30 @@
+const {sqlForPartialUpdate} = require("./sql");
+const { BadRequestError } = require("../expressError");
+
+describe("sqlForPartialUpdate", function () {
+    test("works", function () {
+        const validData = {
+            name: "Apple", 
+            description: "Computers.", 
+            numEmployees: 100
+        }
+        const jsToSql = {numEmployees: "num_employees"};
+        const result = sqlForPartialUpdate(validData, jsToSql);
+
+        expect(result).toEqual({
+            setCols: '"name"=$1, "description"=$2, "num_employees"=$3',
+            values: ["Apple", "Computers.", 100]});
+    });
+
+    test("fails: empty update object", function () {
+        const invalidData = {};
+        const jsToSql = {numEmployees: "num_employees"};
+
+        try {
+            sqlForPartialUpdate(invalidData, jsToSql);
+            throw new Error("fail test");
+        } catch (err) {
+            expect(err instanceof BadRequestError).toBeTruthy();
+        }
+    })
+})
